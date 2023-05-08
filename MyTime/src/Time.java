917949e0659c48c50dd73073d521c49786a7a207
Saitama182 <100026214+Saitama182@users.noreply.github.com>
2023-05-08 15:22:20 +0300

    исправил ошибку в формулах

--- MyTime/src/Time.java
+++ MyTime/src/Time.java
@@ -1,13 +1,17 @@
 public class Time {
     public static void main(String[] args) {
-        int seconds = 300000;
-        int minutes = seconds / 60;
-        int hours = seconds / 3600;
-        int days = seconds / 86400;
-        System.out.println("Секунд: "+ seconds);
-        System.out.println("Минут: "+ minutes);
-        System.out.println("Часов: "+ hours);
+        int seconds = 94162;
+        int secondInMinutes = 60;
+        int secondInHours = secondInMinutes * 60;
+        int secondInDay = secondInHours * 24;
+        int days = seconds / secondInDay;
+        int hours = (seconds % secondInDay) / secondInHours;
+        int minutes = ((seconds % secondInDay) % secondInHours) / secondInMinutes;
+        int leftSeconds = seconds - days * secondInDay - hours * secondInHours - minutes *secondInMinutes;
         System.out.println("Суток: " + days);
+        System.out.println("Часов: " + hours);
+        System.out.println("Минут: " + minutes);
+        System.out.println("Секунд: "+ leftSeconds);
 
 
 

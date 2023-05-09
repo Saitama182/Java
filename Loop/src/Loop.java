e7d2425eacb212b5bec4a49544d8193551954e5f
Saitama182 <100026214+Saitama182@users.noreply.github.com>
2023-05-09 16:47:20 +0300

    задача

---
+++ Loop/src/Loop.java
@@ -0,0 +1,11 @@
+// Вывести все числа от 1 до 1000 , каждое с новой строчки.
+
+public class Loop {
+    public static void main(String[] args) {
+        int i = 1;
+        while ( i <= 1000) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}

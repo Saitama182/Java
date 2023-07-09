05fb5a93c88d1e250d9cd9ff3184b6a1a34d26b2
Saitama182 <100026214+Saitama182@users.noreply.github.com>
2023-07-09 20:59:19 +0300

    HomeWork  for loop

---
+++ for1.3/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int comp = 1 ;
+        for (int i = 1; i < 11; i++) {
+            comp *= i;
+
+        }
+        System.out.println("Произведение  чисел от 1 до 10 равно " + comp);
+    }
+}
---
+++ for1.4/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int [] nums = {1,2,3,4,5,10,100};
+        int sum = 0;
+        for (int i = 0; i< nums.length; i++){
+            sum += nums[i];
+        }
+        System.out.println("Сумма чисел  массива равна " + sum);
+    }
+}

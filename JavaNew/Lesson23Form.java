909e971cf1656c9d863c17884592e3948f391994
Saitama182 <100026214+Saitama182@users.noreply.github.com>
2023-08-23 21:57:01 +0300

    Форматирование строк

---
+++ JavaNew/Lesson23Form.java
@@ -0,0 +1,10 @@
+public class Lesson23Form {
+    public static void main(String[] args){
+        System.out.printf("%fThis is %s a string %d" , 10.4 , "ghrfh" , 1000);
+        System.out.println("");
+        System.out.printf("String %10d%n", 444);
+        System.out.printf("String %10d%n", 4);
+        System.out.printf("String %10d%n", 444444444);
+        System.out.printf("String %.2f", 45.765765765);
+    }
+}

be80059c82f7aab66d4abc8dac4c0fd293ab950e
Saitama182 <100026214+Saitama182@users.noreply.github.com>
2023-08-24 18:33:45 +0300

    туСтринг

---
+++ JavaNew/Lesson24/Less24.java
@@ -0,0 +1,12 @@
+class Less24 {
+    String name;
+    int age;
+
+    public Less24(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public String toString() {
+        return name + " , " + age;
+    }
+}
---
+++ JavaNew/Lesson24/Less24ToString.java
@@ -0,0 +1,8 @@
+public class Less24ToString {
+    public static void main(String[] args){
+        Less24 l1 = new Less24("Bob", 77 );
+
+        System.out.println(l1);
+    }
+    
+}
